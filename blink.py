ee13c93173306598754abee67b094e2529bf918f
bilderkiste <christian@bilderkiste.info>
2025-05-19 09:39:51 +0200

    Update blink.py

--- blink.py
+++ blink.py
@@ -1,6 +1,5 @@
 from machine import Timer, Pin
 from neopixel import NeoPixel
-import time
 
 class ValueChange:
     
@@ -108,21 +107,3 @@ class Neo(OutputDevice):
             
     def set_color(self, color):
         self._color = color
-        
-led = LED(16)
-led.blink(3, 200, 500)
-
-neo2 = Neo(1,0)
-neo2.blink(7, 400, 200)
-
-neo1 = Neo(1, 1)
-neo1.set_color((0,30,100))
-neo1.blink(-1, 100, 200)
-
-time.sleep(5)
-neo1.stop()
-neo2.set_color((20,0,0))
-neo2.blink(7, 400, 200)
-time.sleep(5)
-neo1.set_color((0,30,0))
-neo1.blink(-1, 100, 1000)
