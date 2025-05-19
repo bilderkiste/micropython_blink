6c478d8616aa98b26b299b4cfc0e259bbfe0ee6d
bilderkiste <christian@bilderkiste.info>
2025-05-19 15:40:57 +0200

    Update example.py

--- example.py
+++ example.py
@@ -1,20 +1,18 @@
-from blink import Neo, LED
-from time import sleep
-import machine
+from blink import NeoString, NeoLED, LED
 import time
+import machine
 
 
 led = LED(16)
 led.blink(3, 200, 500)
 
 led = LED("LED")
-led.blink(3, 200, 500)
+led.blink(-1, 200)
 
 neo_string = NeoString(1,4)
-neo_led1 = NeoLED(neo_string,1)
+neo_led1 = NeoLED(neo_string,0)
 neo_led1.set_color((0,100,0))
-neo_led1.blink(4,500,1500)
-neo_led2 = NeoLED(neo_string,0)
-#neo1.set_color((0,100,0))
-neo_led2.blink(4, 1000,1000)
-
+neo_led1.blink(6,100,1500)
+neo_led2 = NeoLED(neo_string,1)
+neo_led2.set_color((0,0,100))
+neo_led2.blink(4)
