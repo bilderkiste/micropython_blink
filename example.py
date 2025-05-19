1ddd768d42c24b7ed0d6f39bc3bf4376690622f8
bilderkiste <christian@bilderkiste.info>
2025-05-19 10:25:51 +0200

    Update example.py
    Add strip length parameter, add pico_led

--- example.py
+++ example.py
@@ -3,13 +3,17 @@ from time import sleep
 import machine
 import time
 
+
 led = LED(16)
 led.blink(3, 200, 500)
 
-neo2 = Neo(1,0)
+led = LED("LED")
+led.blink(3, 200, 500)
+
+neo2 = Neo(1,4,0)
 neo2.blink(7, 400, 200)
 
-neo1 = Neo(1, 1)
+neo1 = Neo(1,4,1)
 neo1.set_color((0,30,100))
 neo1.blink(-1, 100, 200)
 
@@ -19,4 +23,4 @@ neo2.set_color((20,0,0))
 neo2.blink(7, 400, 200)
 time.sleep(5)
 neo1.set_color((0,30,0))
-neo1.blink(-1, 100, 1000)
+neo1.blink(2, 100, 1000)
